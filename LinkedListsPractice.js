947566c894aeb215662bc00496984b89a849174f
WabaScript <rjwahba@gmail.com>
2020-09-14 13:36:44 -0400

    first commit

---
+++ LinkedListsPractice.js
@@ -0,0 +1,7 @@
+// Why use a Linked List? 
+// A linked list is a linear data structure that consists of structs, Nodes, which are linked
+// to one another via pointers.
+// Node consists of the value(any data type) you wish to store and a reference(pointer)
+// A linked list trumps arrays in it's dynamic efficiency. 
+// They are resizable and performant in insertion and deletion.
+// However, cons include slow reads(not sequential in memory, aka,you must traverse to find) and takes up more space(storage).
