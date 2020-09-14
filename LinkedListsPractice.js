3102aca9fbd7a6b68cd37b532c0262beaf5adb00
WabaScript <rjwahba@gmail.com>
2020-09-14 14:28:08 -0400

    simple ref

--- LinkedListsPractice.js
+++ LinkedListsPractice.js
@@ -5,3 +5,16 @@
 // A linked list trumps arrays in it's dynamic efficiency. 
 // They are resizable and performant in insertion and deletion.
 // However, cons include slow reads(not sequential in memory, aka,you must traverse to find) and takes up more space(storage).
+
+// Simple example: 
+// Create two nodes, JS object literals
+
+// const node1 = {
+//     value: 100
+// }
+// const node2 = {
+//     value: 200
+// }
+// Then reference the second inside the first node
+// node1.next = node2;
+// console.log(node1);
